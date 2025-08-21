4b251a58fa210d3e8a44c66ab68eb235663754e9
toddswift <todd.swift@gmail.com>
2025-08-21 12:41:46 -0400

    updated manual query file list for reference

--- InsertTestData/manual.py
+++ InsertTestData/manual.py
@@ -1,4 +1,11 @@
-# learn how to manually query the database
+import sqlite3
+from constants import DATABASE_NAME
+
+# Select all data from the cigar_reviews table
+# open the database from the terminal / command line
+# sqlite3 cigars.db
+# SELECT * FROM cigar_reviews;
 
 
 # learn how to delete from the database
+# DELETE FROM cigar_reviews WHERE id = 1;
